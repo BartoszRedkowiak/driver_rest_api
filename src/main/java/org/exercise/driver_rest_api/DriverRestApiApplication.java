043ab6fcf571f5a2ec0977c377ecd2c040cdba6b
BartoszRedkowiak <b.redkowiak@gmail.com>
2019-12-07 20:25:07 +0100

    Initial commit

---
+++ src/main/java/org/exercise/driver_rest_api/DriverRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.exercise.driver_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/exercise/driver_rest_api/DriverRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exercise.driver_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
